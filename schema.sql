865725c5e74fe223a376940b4b5fddc0596ba269
njorogepeter <pgakonde@yahoo.com>
2019-05-31 14:17:49 -0700

    beginning

---
+++ schema.sql
@@ -0,0 +1,5 @@
+
+-- Drops the localmarket if it exists currently --
+DROP DATABASE IF EXISTS localmarket;
+-- Creates the  localmarket" database --
+CREATE DATABASE localmarket;
---
+++ server.js
@@ -0,0 +1,33 @@
+
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require('express');
+const routes = require('./routes');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+// Requiring our models for syncing
+var db = require('./models');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
