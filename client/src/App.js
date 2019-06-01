0f56ccef8540578178a31f450579b53310c75bdb
njorogepeter <pgakonde@yahoo.com>
2019-06-01 04:05:50 -0700

    added navbar styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Details from './components/Details';
 import Cart from './components/Cart';
@@ -24,6 +25,9 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
+            <Route exact path="/productList" component={ProductList} />
+            <Route exact path="/details" component={Details} />
+            <Route exact path="/cart" component={Cart} />
             <Route component={NoMatch} />
           </Switch>
         </div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,38 @@
 import React, { useContext } from 'react';
 import { CounterContext } from '../context';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button';
+import logo from '../logo.svg';
 
 const Navbar = () => {
   const { currentUser } = useContext(CounterContext);
   return (
     <div>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Login</Link>This is your new navbar{' '}
+    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <img src={logo} alt="store" className="Navbar-brand" /> 
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>This is your new navbar{' '}
+      <Link to="/cart" className="ml-auto">
+        <ButtonContainer>
+          <i className="fas fa-cart-plus" />
+          checkout
+        </ButtonContainer>
+      </Link>
+    </NavWrapper>
+   
+      
+
     </div>
   );
 };
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainwhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
 
 export default Navbar;
